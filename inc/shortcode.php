b08f794fcf031aab8eec139fca4def8362a8b4a7
Alexandr K. <elapsekap@gmail.com>
2025-03-03 19:22:47 +0200

    removed filtered products function by price; removed WC_Product_Query; for the product selecting was used standard WP_Query class with price range settings;

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -6,28 +6,60 @@ if ( ! defined( 'ABSPATH') ) {
 }
 
 
-function slider_shortcode_products_slider_shortcode( $attr ) {
+function slider_shortcode_products_slider_shortcode( $attr = array() ) {
 	$content = '';
 
 	if ( ! class_exists( 'WC_Product_Query' ) ) {
 		return $content;
 	}
 
-	$args    = array(
-		'limit' => -1,
-		'orderby' => 'date',
-		'order' => 'DESC',
-		'status' => 'publish'
+	$args = array(
+		'post_type'      => 'product',
+		'post_status'    => 'publish',
+		'posts_per_page' => -1,
+		'orderby'        => 'DESC',
+		'fields'         => 'ids',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'product_type',
+				'field'    => 'slug',
+				'terms'    => array( 'grouped' ),
+				'operator' => 'NOT IN'
+			)
+		)
 	);
-	$products = ( new WC_Product_Query( $args ) )->get_products();
-	$products = slider_shortcode_filter_products_by_price( $products, $attr );
 
-	$section_title = $attr['title'] ?? null;
-	$template_path = SC_PLUGIN_PATH . '/templates/products-slider.php';
-	if ( file_exists( $template_path ) ) {
-		ob_start();
-		include_once $template_path;
-		$content = ob_get_clean();
+	foreach( $attr as $item => $val ) {
+		if ( ! in_array( $item, array( 'min-price', 'max-price' ) ) ) {
+			continue;
+		}
+
+		$args['meta_query'][] = array(
+			'key'     => '_price',
+			'value'   => $val,
+			'compare' => 'min-price' == $item ? '>=' : '<=',
+			'type'    => 'NUMERIC'
+		) ;
+	}
+
+	if ( key_exists( 'meta_query', $args ) && 2 == count( $args['meta_query'] ) ) {
+		$args['meta_query']['relation'] = 'AND';
+	}
+
+	$query = new WP_Query( $args );
+	if ( $query->have_posts() ) {
+		$products = array_map(
+			fn( $prod_id ) => wc_get_product( $prod_id ),
+			$query->get_posts()
+		);
+
+		$section_title = $attr['title'] ?? null;
+		$template_path = SC_PLUGIN_PATH . '/templates/products-slider.php';
+		if ( file_exists( $template_path ) ) {
+			ob_start();
+			include_once $template_path;
+			$content = ob_get_clean();
+		}
 	}
 
 	return $content;
